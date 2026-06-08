2c29fdb909ffad356579b5394c1cab87375d3bb7
anhquan7826335 <lquankk@gmail.com>
2026-06-08 23:59:52 +0700

    add firebase

--- app/lib/services/firebase_database_service.dart
+++ app/lib/services/firebase_database_service.dart
@@ -1,5 +1,4 @@
-// lib/services/firebase_database_service.dart
-// ✅ Thay thế appwrite_database_service.dart
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 /// Collection paths trong Firestore
